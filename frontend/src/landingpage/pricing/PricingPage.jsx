5098499c28df43954101c6ab6539ee74555b982d
Chirag Saini <sainischirag@gmail.com>
2026-09-03 09:46:01 +0530

    initial display structure

---
+++ frontend/src/landingpage/pricing/PricingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Brokerage from './Brokerage';
+import Hero from './Hero';
+function PricingPage() {
+    return ( <h1>
+        <Brokerage/>
+        <Hero/> 
+    </h1> );
+}
+
+export default PricingPage;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import HomePage from './landingpage/home/HomePage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>hello</div>
+    <HomePage/>
   </StrictMode>,
 )
