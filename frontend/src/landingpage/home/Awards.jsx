b414b2e6b174d2bb1e7efde864dc00e42833277d
Chirag Saini <sainischirag@gmail.com>
2026-09-05 15:35:06 +0530

    Home/Hero and Awards

--- frontend/src/landingpage/home/Awards.jsx
+++ frontend/src/landingpage/home/Awards.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className="row">
+                <div className="col-lg-6 col-sm-12">
+                <img src="media/images/largestBroker.svg" alt="" />
+                </div>
+                <div style={{width:'42%'}} className="col-lg-6">
+                    <h1>Largest stock broker in India</h1>
+                    <p>2+ million CandleEdge clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <div  className="row mt-5">
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Futures and Options</p></li>
+                                <li><p>Stocks & IPOs</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li><p>Direct mutual funds</p></li>
+                                <li><p>Direct mutual funds</p></li>
+                                <li><p>Bonds and Govt. securities</p></li>
+                            </ul>
+                        </div>
+                        <img style={{width:"90%"}} src="media/images/pressLogos.png" alt="" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
      );
 }
 
